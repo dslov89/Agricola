d55a5c101271f11d616eeea37aa79fd1a7b686cc
yangjaehyuk <gndksakxk10@naver.com>
2023-05-23 20:49:46 +0900

    turn api update

--- src/components/GameRoomBoard.js
+++ src/components/GameRoomBoard.js
@@ -3,57 +3,49 @@ import { useNavigate } from "react-router-dom";
 import * as SockJS from "sockjs-client";
 import * as Stomp from "@stomp/stompjs";
 import axios from "axios";
-import { useSyncExternalStore } from "react";
-// import * as StompJs from '@stomp/stompjs';
-// user 가 socket 통신 신청하면 유저한테 id 부여해주는거랑 게임방 들어갈 때 만약 인원수 4명이면 못들어가게 하고 그 이하면 들어가게 해주는 코드 작성
-
 const Gameroomboard = () => {
-  axios.post("http://localhost:8080/game-rooms", null, {
-    params: {
-      roomId: 1,
-    },
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
   const client = useRef();
   const navigation = useNavigate();
+  const [rooms, setRooms] = useState([]);
   function naviHandler() {
     navigation("/start");
   }
-  const connectHandler = () => {
+
+  const connectHandler = (roomId) => {
     client.current = Stomp.Stomp.over(() => {
       const sock = new SockJS("http://localhost:8080/enter");
       return sock;
     });
     client.current.connect({}, () => {
-      // callback 함수 설정, 대부분 여기에 sub 함수 씀
       client.current.subscribe(
-        `/user/sub/game-room/1`,
+        `/user/sub/game-room/` + roomId,
         (message) => {
           console.log(message.body);
-          if (message.body === "OK") {
+          if (message.body === "OK" + ' ' + 1) { //turn example
+            client.current.subscribe(
+              `/sub/game-room/` + roomId,
+              (message) => {
+                console.log(message.body);
+              }
+            )
+            localStorage.setItem("turn", message.body[3]);
+            console.log(localStorage.getItem("turn"));
             naviHandler();
-          }
-          else {
+          } else {
             alert("게임에 진입할 수 없습니다.");
           }
         },
-        { gameRoomId: 1 }
+        { gameRoomId: roomId }
       );
     });
   };
 
-  const sendHandler = () => {
-    // 서버에서 사람 없으면 "OK" 4명이면 "FULL"이런식으로 메세지 보낼꺼에요
+  const sendHandler = (roomId) => {
     client.current.send(
       "/main-board/user/init",
       {},
       JSON.stringify({
-        roomId: 1,
+        roomId: roomId,
         userId: 2,
         action: [1, 2, 3],
         content: "hello",
@@ -61,13 +53,12 @@ const Gameroomboard = () => {
     );
   };
 
-  const testHandler = () => {
-    // game-room/id 구독하고 있는 사람들한테 전부 메세지 보내는거에요
+  const testHandler = (roomId) => {
     client.current.send(
       "/main-board/user/test",
       {},
       JSON.stringify({
-        roomId: 1,
+        roomId: roomId,
         userId: 2,
         action: [1, 2, 3],
         content: "hello",
@@ -75,13 +66,66 @@ const Gameroomboard = () => {
     );
   };
 
+  const createRoom = (roomId) => {
+    axios.post("http://localhost:8080/game-rooms", null, {
+        params: {
+          roomId: roomId,
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  // 방 목록 불러와서 중복되는 방 있으면 생성하지 않음(현재 이대로 하면 방이 생성이 안됨,,)
+
+  // useEffect(() => {
+  //     if (rooms.length > 0) {
+  //       createRoom(rooms[rooms.length - 1]);
+  //     }
+  // }, [rooms]);
+  
+    useEffect(() => {
+      if (rooms.length > 0) {
+        const lastRoomId = rooms[rooms.length - 1];
+        const checkRoomExists = async () => {
+          try {
+            const response = await axios.get(
+              `http://localhost:8080/game-rooms/${lastRoomId}`
+            );
+            const roomExists = response.data.length > 0;
+            console.log(roomExists);
+            if (roomExists) {
+              console.log(`Room ${lastRoomId} already exists.`);
+            }
+          } catch (error) {
+            console.log(error);
+            createRoom(lastRoomId);
+          }
+        };
+        checkRoomExists();
+      }
+    }, [rooms]);
+  const handleCreateRoom = () => {
+    const newRoomId = rooms.length + 1;
+    setRooms((prevRooms) => [...prevRooms, newRoomId]);
+  };
   return (
     <div>
-      <h1>GameRoom</h1>
-      <button onClick={connectHandler}>게임 연결</button>
-      <button onClick={sendHandler}>게임 입장</button>
-      <button onClick={testHandler}>test message</button>
+      <button onClick={handleCreateRoom}>방 생성하기</button>
+      {rooms.map((roomId) => (
+        <div key={roomId}>
+          <h1>GameRoom {roomId}</h1>
+          <button onClick={() => connectHandler(roomId)}>게임 연결</button>
+          <button onClick={() => sendHandler(roomId)}>게임 입장</button>
+          <button onClick={() => testHandler(roomId)}>test message</button>
+        </div>
+      ))}
     </div>
   );
 };
+
 export default Gameroomboard;
