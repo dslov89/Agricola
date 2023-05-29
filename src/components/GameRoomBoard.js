d710b91d29176958b5c9585f542ebcb399b8594d
ingssg <js2121good@gmail.com>
2023-05-30 00:40:04 +0900

    getRooms updated

--- src/components/GameRoomBoard.js
+++ src/components/GameRoomBoard.js
@@ -7,7 +7,6 @@ import { nameValue, sendingClient } from "../screen/Start";
 const Gameroomboard = () => {
   const navigation = useNavigate();
   const [rooms, setRooms] = useState([]);
-  let roomID;
 
   function naviHandler() {
     navigation("/start");
@@ -67,42 +66,36 @@ const Gameroomboard = () => {
   const createRoom = () => {
     axios.post("http://localhost:8080/game-rooms", null, {})
       .then(function (response) {
-        console.log(response.data);
-        roomID = response.data;
+        setRooms((prevRooms) => [...prevRooms, response.data]);
       })
       .catch(function (error) {
         console.log(error);
       });
   };
+
   const getRooms = () => {
     axios.get("http://localhost:8080/game-rooms").then((response) => {
-      console.log("getRoom 쓰는중")
-      console.log(response.data);
-      console.log("룸 개수");
       const roomData = response.data;
       const roomArray = roomData.map((gameroomid) => gameroomid.id);
       setRooms(roomArray);
-      console.log("룸 데이터");
-      console.log(roomArray);
     });
   }
 
   const checkRoomExists = async () => {
-    let lastRoomId = rooms[rooms.length - 1] + 1;
-    let newRoomId = rooms.length+1;
+    let lastRoomId = 1;
+    if (rooms.length !== 0) {
+      lastRoomId = rooms[rooms.length - 1] + 1;
+    }
     try {
       const response = await axios.get(
         `http://localhost:8080/game-rooms/${lastRoomId}`
       );
-      console.log(response.data);
       if (response.data) {
-        console.log(`Room ${lastRoomId} already exists.`);
+        alert("중복된 방이 있습니다. 새로고침을 진행합니다.");
+        getRooms();
       }
     } catch (error) {
-      console.log(error);
-      console.log(lastRoomId);
       createRoom();
-      setRooms((prevRooms) => [...prevRooms, newRoomId]);
     }
   };
 
@@ -122,10 +115,10 @@ const Gameroomboard = () => {
     <div>
       <button onClick={checkRooms}>방 불러오기</button>
       <button onClick={handleCreateRoom}>방 생성하기</button>
-      {rooms.map((newRoomId) => (
-        <div key={newRoomId}>
-          <h1>GameRoom {newRoomId}</h1>
-          <button onClick={() => enterRoom(newRoomId)}>게임 입장</button>
+      {rooms.map((ID) => (
+        <div key={ID}>
+          <h1>GameRoom {ID}</h1>
+          <button onClick={() => enterRoom(ID)}>게임 입장</button>
         </div>
       ))}
     </div>
