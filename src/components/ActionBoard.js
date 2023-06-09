14d1e3dc136a6ce10b7cf533ac6b59d0f00eef43
ingssg <js2121good@gmail.com>
2023-06-10 01:22:55 +0900

    Merge: 곡식 활용 빵굽기 선택 여부에 따른 행동 추가

--- src/components/ActionBoard.js
+++ src/components/ActionBoard.js
@@ -929,25 +929,23 @@ function ActionBoard({ data, setData }) {
 
     let plow_count = userData[`user${farmData.turn}`].farm_array.filter((item) => item === "plow").length;
     if (plow_count>0 && (userData[`user${farmData.turn}`].grain>0 || userData[`user${farmData.turn}`].vegetable>0)) {
-      if (farmData.action[18][0] === 0) {
+      if (farmData.action[18][0] === 0) { // 아무도 곡식 활용 안 눌렀으면
+
         const mainCard = userData[`user${farmData.turn}`].main;
         if(mainCard.includes(1) || mainCard.includes(2) || mainCard.includes(3)   // 해당 메인설비들 내려놨으면 빵굽기 가능
-      || mainCard.includes(4) || mainCard.includes(6) || mainCard.includes(7))  // 1,2 : 화로 //3,4 : 화덕// 6 : 흙가마// 7: 돌가마
-      {                                                                         // (사진 이름으로 인덱스 매겼어요)
-        
-        if(window.confirm("빵굽기를 하시겠습니까?")) {
-          setIsBake(true); //  이 놈 true 되면 보유하고 있는 메인 설비 목록 띄워주기
-          // 각 조건에 따라서 빵굽기 해줘야 함
-          // 보유하고 있는 메인 설비 클릭 가능하게 해주면 되지 않을까 생각 중
-          
-          } else {
-          // 빵 안구우니까 그냥 턴 넘겨주기
-          }
-      }
+        || mainCard.includes(4) || mainCard.includes(6) || mainCard.includes(7))  // 1,2 : 화로 //3,4 : 화덕// 6 : 흙가마// 7: 돌가마
+        {                                                                         // (사진 이름으로 인덱스 매겼어요)
+          if(window.confirm("빵굽기를 하시겠습니까?")) {
+            setIsBake(true); //  이 놈 true 되면 보유하고 있는 메인 설비 목록 띄워주기
+                              // 각 조건에 따라서 빵굽기 해줘야 함
+                              // 보유하고 있는 메인 설비 클릭 가능하게 해주면 되지 않을까 생각 중
+            
+            } else {
+            // 빵 안구우니까 곡식 활용만 하고 넘겨주기
+            }
+        }
 
-
-
-
+       // 직업 관련 로직
         if(userData[`user${farmData.turn}`].job.includes(1)) { // 직업 01. 장작 채집자
           const newdata = {...userData};
           newdata[`user${farmData.turn}`].tree += 1;
